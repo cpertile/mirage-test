a6959cf21df48cd5a78d9d4bfb6d5a7153a81f79
cpertile <c.pertile@icloud.com>
2020-02-10 20:51:25 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
-function App() {
+const App = () => {
+  const [users, setUsers] = useState([]);
+
+  /* It's possible to use 'fetch' or 'axios' */
+
+  // useEffect(() => {
+  //   fetch('https://my.api.mockaroo.com/mockaroo.json?key=51425750')
+  //     .then(res => res.json())
+  //     .then(json => setUsers(json.users))
+  // }, [])
+
+  useEffect(() => {
+    axios.get('https://my.api.mockaroo.com/mockaroo.json?key=51425750')
+    .then(res => setUsers(res.data.users))
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+      {users.map(user => (
+        <li key={user.id}>
+          {user.first_name + ' ' + user.last_name + ' - ' + user.email}
+        </li>
+      ))}
+    </ul>
   );
 }
 
---
+++ src/server.js
@@ -0,0 +1,31 @@
+import { Server, Model } from 'miragejs'
+
+export function makeServer({ environment = 'development' } = {}) {
+  const server = new Server({
+    environment,
+
+    models: {
+      user: Model,
+    },
+
+    seeds(server) {
+      server.create('User', { 'id': 1, 'first_name': 'Mirage Stormy', 'last_name': 'Dimic', 'email': 'sdimic0@friendfeed.com' })
+      server.create('User', { 'id': 2, 'first_name': 'Mirage Jewell', 'last_name': 'Zanettini', 'email': 'jzanettini1@netscape.com' })
+      server.create('User', { 'id': 3, 'first_name': 'Mirage Ameline', 'last_name': 'Saberton', 'email': 'asaberton2@amazonaws.com' })
+      server.create('User', { 'id': 4, 'first_name': 'Mirage Tore', 'last_name': 'Atling', 'email': 'tatling3@census.gov' })
+      server.create('User', { 'id': 5, 'first_name': 'Mirage Forester', 'last_name': 'Wheatcroft', 'email': 'fwheatcroft4@indiatimes.com' })
+    },
+
+    routes() {
+      this.namespace = ''
+
+      this.get('https://my.api.mockaroo.com/mockaroo.json', (schema, request) => {
+        console.log('param key=' + request.queryParams.key)
+
+        return schema.users.all()
+      })
+    },
+  })
+
+  return server
+}
